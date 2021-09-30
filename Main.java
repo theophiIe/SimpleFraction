0e34135fd7c5fd3917d432a4edc1b3e8d6054627
theophiIe <theomolinatti@gmail.com>
2021-09-30 16:29:59 +0200

    Ajout d'un test pour la classe Fraction

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("TD1");
+
+        var fraction = new Fraction(1, 2);
+        System.out.println(fraction.toString());
     }
 }
